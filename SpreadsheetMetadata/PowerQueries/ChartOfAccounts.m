7be88ea889d35d7f74433d5be597d34473d201f8
Charl Potgieter <charl.j.potgieter@gmail.com>
2020-05-30 18:16:00 +1000

    Add account sort orders

--- SpreadsheetMetadata/PowerQueries/ChartOfAccounts.m
+++ SpreadsheetMetadata/PowerQueries/ChartOfAccounts.m
@@ -5,6 +5,6 @@ let
             [Account Code and Description], 
             Text.Length([Account Code and Description]) -param_NumberOfAccountDigits -1)
         , type text),
-    Reorder = Table.ReorderColumns(AddAccountDescription,{"Account Code", "Account Description", "Account Code and Description", "Account Category 1", "Account Category 2", "Account Category 3", "Account Category 4", "Account Category 5"})
+    Reorder = Table.ReorderColumns(AddAccountDescription,{"Account Code", "Account Description", "Account Code and Description", "Account Category 1", "Account Category 2", "Account Category 3", "Account Category 4", "Account Category 5", "Sort Order Account Category 1", "Sort Order Account Category 2", "Sort Order Account Category 3", "Sort Order Account Category 4", "Sort Order Account Category 5"})
 in
     Reorder
--- SpreadsheetMetadata/PowerQueries/DataAccess_ChartOfAccounts.m
+++ SpreadsheetMetadata/PowerQueries/DataAccess_ChartOfAccounts.m
@@ -1,5 +1,5 @@
 let
     Source = Excel.CurrentWorkbook(){[Name="tbl_InputChartOfAccounts"]}[Content],
-    ChangedType = Table.TransformColumnTypes(Source,{{"Account Code and Description", type text}, {"Account Category 1", type text}, {"Account Category 2", type text}, {"Account Category 3", type text}, {"Account Category 4", type text}, {"Account Category 5", type text}})
+    ChangedType = Table.TransformColumnTypes(Source,{{"Account Code and Description", type text}, {"Account Category 1", type text}, {"Account Category 2", type text}, {"Account Category 3", type text}, {"Account Category 4", type text}, {"Account Category 5", type text}, {"Sort Order Account Category 1", Int64.Type}, {"Sort Order Account Category 2", Int64.Type}, {"Sort Order Account Category 3", Int64.Type}, {"Sort Order Account Category 4", Int64.Type}, {"Sort Order Account Category 5", Int64.Type}})
 in
     ChangedType
